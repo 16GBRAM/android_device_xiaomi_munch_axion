580661308ef9a4c2d05db49ef3b97d33fc195159
Bruno Martins <bgcngm@gmail.com>
2025-11-26 17:53:55 +0100

    munch: parts: SettingsFragment: Defer applying divider
    Android 16 QPR1 + M3E changes the inflation and lifecycle timing of
    PreferenceFragmentCompat layouts, resulting in internal RecyclerView
    being created later than before.
    
    As a result, PreferenceFragmentCompat.setDivider() can be invoked
    before the internal RecyclerView exists thus causing below NPE:
    
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: FATAL EXCEPTION: main
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: Process: org.lineageos.lineageparts, PID: 7903
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void androidx.recyclerview.widget.RecyclerView.invalidateItemDecorations()' on a null object reference
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at androidx.preference.PreferenceFragmentCompat$DividerDecoration.setDivider(PreferenceFragmentCompat.java:909)
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at androidx.preference.PreferenceFragmentCompat.setDivider(PreferenceFragmentCompat.java:247)
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at com.android.settingslib.widget.SettingsBasePreferenceFragment.onViewCreated(SettingsBasePreferenceFragment.kt:47)
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at org.lineageos.lineageparts.SettingsPreferenceFragment.onViewCreated(SettingsPreferenceFragment.java:156)
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at org.lineageos.lineageparts.profiles.SetupTriggersFragment.onViewCreated(SetupTriggersFragment.java:89)
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at androidx.fragment.app.Fragment.performViewCreated(Fragment.java:3099)
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at androidx.fragment.app.FragmentStateManager.createView(FragmentStateManager.java:607)
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at androidx.fragment.app.FragmentStateManager.moveToExpectedState(FragmentStateManager.java:285)
      (...)
     # 11-25 18:06:19.705 7903 7903 E AndroidRuntime: at android.app.ActivityThread.main(ActivityThread.java:9041)
     # 11-25 18:06:19.705 7903 7903 E AndroidRuntime: at java.lang.reflect.Method.invoke(Native Method)
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:593)
      11-25 18:06:19.705 7903 7903 E AndroidRuntime: at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:929)
      11-25 18:06:19.727 2241 6629 W ActivityTaskManager: Force finishing activity org.lineageos.lineageparts/.PartsActivity
    
    Test: Open System profiles settings, create a new profile and verify
          that the activity no longer crashes.
    Change-Id: Ifb741011baaa6d721d0ca0db2d68d30037eb8930
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Kenaidi01 <andrianp920@gmail.com>

--- parts/src/org/lineageos/settings/thermal/ThermalSettingsFragment.java
+++ parts/src/org/lineageos/settings/thermal/ThermalSettingsFragment.java
@@ -15,13 +15,13 @@
  */
 package org.lineageos.settings.thermal;
 
-import android.annotation.Nullable;
 import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.TypedValue;
@@ -37,6 +37,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import com.android.settingslib.widget.SettingsBasePreferenceFragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -98,6 +99,19 @@ public class ThermalSettingsFragment extends SettingsBasePreferenceFragment
         mAppsRecyclerView.setAdapter(mAllPackagesAdapter);
     }
 
+    @Override
+    public void setDivider(@Nullable final Drawable divider) {
+        RecyclerView list = getListView();
+        if (list == null) {
+            View root = getView();
+            if (root != null) {
+                root.post(() -> setDivider(divider));
+            }
+            return;
+        }
+
+        super.setDivider(divider);
+    }
 
     @Override
     public void onResume() {
